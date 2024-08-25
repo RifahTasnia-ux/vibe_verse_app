cd6b4bd4c78e1e324d67d8a3edfca6c1c38755c6
Tamim Chowdhury <tamimchowdhury109@gmail.com>
2024-08-26 03:34:15 +0600

    SOC-8: Updated Login UI design

--- lib/presentation/screens/auth/landing_screen.dart
+++ lib/presentation/screens/auth/landing_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:vibe_verse/presentation/screens/home_screen/home_screen.dart';
+import 'package:vibe_verse/presentation/screens/auth/registration_screen.dart';
 import 'package:vibe_verse/utils/app_colors.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
@@ -51,7 +51,7 @@ class LandingScreen extends StatelessWidget {
                     Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => const HomeScreen(),
+                        builder: (context) => const SignUpScreen(),
                       ),
                     );
                   },
